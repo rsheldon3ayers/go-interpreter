284193a9975b7c3f5a91478756629142a2c09c6e
Robert Ayers <rsheldon3ayers@gmail.com>
2024-08-11 11:41:23 -0400

    error handling.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -17,6 +17,8 @@ let foobar = 838383;
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -39,7 +41,6 @@ let foobar = 838383;
 			return
 		}
 	}
-
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
@@ -57,4 +58,19 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		return false
 	}
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+
 }
